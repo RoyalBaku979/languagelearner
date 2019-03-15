81f68c77364610b9ef25fd78eb1cbb43b6d486b8
Sarkhan Rasullu <sarkhanrasullu@gmail.com>
2019-03-15 23:40:56 +0400

    web project

---
+++ languagelearnerwebappnew/src/main/java/servlets/UserServlet.java
@@ -0,0 +1,75 @@
+package servlets;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.company.Context;
+import com.company.dao.inter.UserDaoInter;
+import com.company.entity.User;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sarkhanrasullu
+ */
+@WebServlet(urlPatterns = {"/UserServlet"})
+public class UserServlet extends HttpServlet {
+
+    private UserDaoInter userDao = Context.instanceUserDao();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UserServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            List<User> users = userDao.getAll();
+            for(int i=0;i<users.size();i++) {
+                User u = users.get(i);
+                out.println("<h1>"+u.getName()+" "+u.getSurname()+"</h1>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UserServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("You requested to post");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
---
+++ languagelearnerwebappnew/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Salam</title>
+    </head>
+    <body>
+        Hello World!
+    </body>
+
+</html>
